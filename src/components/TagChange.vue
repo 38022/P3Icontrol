68854aa64e91243a853bf18d276c9152581e9289
ishima <nozomi.kasho@konicaminolta.com>
2025-07-22 14:43:03 +0900

    タグ変更ページで編集部表示される

--- src/components/TagChange.vue
+++ src/components/TagChange.vue
@@ -9,7 +9,47 @@
         </option>
       </select>
     </div>
-
+    <button @click="showEdit = true" :disabled="!selectedTag">OK</button>
+    <!-- OKボタンがクリックされると編集部分が表示される -->
+    <!-- 何かしら選択していないとOKボタンは押せない -->
+    <div v-if="showEdit" class="edit-tag">
+      <div class="edit-factory">
+        <label>工場：</label>
+        <select v-model="selectedFactory">
+          <option v-for="factory in factoryList" :key="factory" :value="factory">
+            {{ factory }}
+          </option>
+        </select>
+      </div>
+      <div class="edit-zone">
+        <label>ゾーン：</label>
+        <select v-model="selectedZone">
+          <option v-for="zone in zoneList" :key="zone" :value="zone">
+            {{ zone }}
+          </option>
+        </select>
+      </div>
+      <div class="edit-subject">
+        <label>被写体名称：</label>
+        <input list="subjects" v-model="selectedSubject" />
+        <datalist id="subjects">
+          <option v-for="subjects in subjectsList" :key="subjects" :value="subjects">
+            {{ subjects }}
+          </option>
+        </datalist>
+      </div>
+      <div class="edit-side">
+        <label>位置：</label>
+        <select v-model="selectedSide">
+          <option>DS</option>
+          <option>OS</option>
+        </select>
+      </div>
+      <div class="edit-memo">
+        <label>メモ：</label>
+        <input v-model="memo" type="text" placeholder="必須"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +59,16 @@ export default {
   data() {
     return {
       tagsList: ['L4_b_p3_001_004_テンター出ツレ_Th250_DS_PV'],
-      selectedTag: ''
+      selectedTag: '',
+      selectedFactory: '',
+      factoryList: ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8'],
+      selectedZone: '',
+      zoneList: ['a', 'b'],
+      selectedSubject: '',
+      subjectsList: ['テンター出ツレ', 'テンター入り'],
+      selectedSide: '',
+      memo: '',
+      showEdit: false
     }
   }
 }
@@ -55,5 +104,13 @@ h2::after {
 }
 .select-tag select {
   width: 330px;
+}
+.edit-tag {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 50px;
+  margin-top: 60px;
+  gap: 10px;
 }
 </style>
