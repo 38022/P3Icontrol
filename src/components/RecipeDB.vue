39b6485c11854dac23affcdbe263c75b43d10a7a
ishima <nozomi.kasho@konicaminolta.com>
2025-08-08 17:23:43 +0900

    レシピDB操作画面改良

--- src/components/RecipeDB.vue
+++ src/components/RecipeDB.vue
@@ -7,8 +7,65 @@
       <router-link to="/">ホーム</router-link>
       <span> &gt;</span>
       <span>レシピDB操作</span>
-    </nav>    
-    <div class="button-group">
+    </nav>
+    <div class="select-factory">
+      <label class="select-label" for="factor-select">工場を選択</label>
+      <div class="select-row">
+        <select id="factory-select" v-model="selectedFactory">
+          <option v-for="factory in factories" :key="factory" :value="factory">{{ factory }}</option>
+        </select>
+        <button @click="onFactoryOk">OK</button>
+        <!-- 工場を選択しOKボタンをクリックしたら各工場のテーブルが表示される -->
+      </div>
+    </div>    
+    <div class="recipe-log" v-if="showLog">
+      <table>
+        <thead>
+          <tr>
+            <th>項番</th>
+            <th>レシピID</th>
+            <th>カメラ名</th>
+            <th>最終編集日時</th>
+            <th>作成日時</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, i) in recipeList" :key="i">
+            <td>{{ row.no }}</td>
+            <td>{{ row.recipeId }}</td>
+            <td>{{ row.camera }}</td>
+            <td>{{ row.editDate }}</td>
+            <td>{{ row.createDate }}</td>
+            <td>
+              <router-link
+              to="/recipeDB/recipeChange">編集</router-link>
+            </td>
+            <td>
+              <!-- recipeIdがnullなら有効、cameraがnullなら無効 -->
+              <router-link
+                v-if="row.recipeId === null && row.camera !== null"
+                to="/recipeDB/recipeAdd"
+              >追加</router-link>
+              <span v-else-if="row.camera === null" style="color: #ccc; pointer-events: none; cursor: default;">追加</span>
+              <span v-else style="color: #ccc; pointer-events: none; cursor: default;">追加</span>
+            </td>
+            <td>
+              <!-- cameraがnullなら有効、recipeIdがnullなら無効 -->
+              <router-link
+                v-if="row.camera === null && row.recipeId !== null"
+                to="/recipeDB/recipeDelete"
+              >削除</router-link>
+              <span v-else-if="row.recipeId === null" style="color: #ccc; pointer-events: none; cursor: default;">削除</span>
+              <span v-else style="color: #ccc; pointer-events: none; cursor: default;">削除</span>
+            </td>
+            <td>
+              <router-link to="/recipeDB/recipeHistory">編集履歴</router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- <div class="button-group">
       <router-link to="/recipeDB/recipeHistory" class="menu-item-link">
         <button class="menu-item">
           <img src="@/assets/log_icon.png" alt="レシピ更新履歴" />
@@ -27,9 +84,52 @@
           <span>レシピ変更</span>
         </button>
       </router-link>
-  </div>
+    </div> -->
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SystemSetting',
+  data() {
+    return {
+      selectedFactory: 'L1',
+      displayFactory: null,
+      factories: ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7'],
+      showLog: false,
+      recipeList: [
+        {
+          no: '1',
+          recipeId: 'recipe_id3',
+          camera: 'CAM1',
+          editDate: '2025/08/01 12:00',
+          createDate: '2025/07/22 12:00'
+        },
+        {
+          no: '2',
+          recipeId: null,
+          camera: 'CAM2',
+          editDate: '2025/08/01 12:00',
+          createDate: '2025/07/22 12:00'
+        },
+        {
+          no: '3',
+          recipeId: 'recipe_id5',
+          camera: null,
+          editDate: '2025/08/01 12:00',
+          createDate: '2025/07/22 12:00'
+        }
+      ]
+      }
+    },
+  methods: {
+    onFactoryOk() {
+      this.displayFactory = this.selectedFactory;
+      this.showLog = true;
+    }
+  }
+}
+</script>
 
 <style scoped>
 .breadcrumb {
@@ -75,6 +175,74 @@
   text-align: left;
 }
 
+.select-factory {
+  margin-top: 32px;
+  margin-left: 30px;
+  margin-bottom: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.select-label {
+  font-size: 1.1rem;
+  margin-bottom: 8px;
+  margin-left: 0;
+}
+
+.select-row {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 30px;
+}
+
+select {
+  padding: 6px 16px;
+  font-size: 1rem;
+}
+
+button {
+  padding: 4px 10px;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+.recipe-log {
+  padding-left: 40px;
+  padding-right: 40px;
+  box-sizing: border-box;
+}
+.recipe-log table {
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 32px;
+}
+
+.recipe-log th,
+.recipe-log td {
+  border: 1px solid #888;
+  padding: 8px 12px;
+  text-align: center;
+  background: #fff; /* セルの背景を白に */
+  font-size: 1rem;
+}
+
+.recipe-log th {
+  background: #fff;
+  font-weight: bold;
+}
+
+.recipe-log a,
+.recipe-log a:visited {
+  color: #1976d2;
+  text-decoration: underline;
+}
+.recipe-log a:hover {
+  text-decoration: underline;
+  color: #1256a0;
+}
+/* 
 .button-group {
   display: flex;
   flex-direction: row;
@@ -114,6 +282,6 @@
 .menu-item img {
   width: 48px;
   height: 48px;
-}
+} */
 
 </style>
